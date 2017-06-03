47a64bcf47067d96c8d73a90d3837ef1c467b6b8
ahiru <wellingtonnmaeda0524@gmail.com>
2017-06-03 21:55:26 +0900

    多分いらないであろうものを削除（破壊）
    `Message#post` が今の推奨なので投稿部分をそれに書き換え。
    mediaiolist は名前の通りリストなので、単一のファイルを開いてそいつを `[]` で配列にぶち込んでる。

--- d250g2_emoji.rb
+++ d250g2_emoji.rb
@@ -1,35 +1,6 @@
 # -*- coding: utf-8 -*-
-require 'twitter'
 
 Plugin.create(:d250g2) do
-  @clients = {}
-
-  unless UserConfig[:twitter_secret] # mikutter >= 3.0.0
-    @clients[Service.primary.idname] = Twitter::REST::Client.new do |c|
-      c.consumer_key = Service.primary.twitter.consumer_key
-      c.consumer_secret = Service.primary.twitter.consumer_secret
-      c.oauth_token = Service.primary.twitter.a_token
-      c.oauth_token_secret = Service.primary.twitter.a_secret
-    end
-  else # mikutter < 3.0.0
-    if defined? Twitter::REST
-      @clients[Service.primary.idname] = Twitter::REST::Client.new do |c|
-        c.consumer_key = CHIConfig::TWITTER_CONSUMER_KEY
-        c.consumer_secret = CHIConfig::TWITTER_CONSUMER_SECRET
-        c.oauth_token = UserConfig[:twitter_token]
-        c.oauth_token_secret = UserConfig[:twitter_secret]
-      end
-    else
-      Twitter.configure do |c|
-        c.consumer_key = CHIConfig::TWITTER_CONSUMER_KEY
-        c.consumer_secret = CHIConfig::TWITTER_CONSUMER_SECRET
-        c.oauth_token = UserConfig[:twitter_token]
-        c.oauth_token_secret = UserConfig[:twitter_secret]
-      end
-      @clients[Service.primary.idname] = Twitter.client
-    end
-  end
-
   begin
     @reply_array = YAML.load_file(File.join(__dir__, 'config.yml'))
   rescue LoadError
@@ -51,8 +22,10 @@ Plugin.create(:d250g2) do
     message = "@#{id}"
     filename = @reply_array.sample
     Thread.new {
-      @clients[Service.primary.idname].update_with_media(message, File.new(File.join(__dir__, 'emoji', filename)))
+      m.post(message: message,
+             mediaiolist: [File.new(File.join(__dir__, 'emoji', filename))])
+    }.trap { |err|
+      error err
     }
   end
-
 end
